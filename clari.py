958b77f5ea99b9bf4a236f82bcb4153e09a29947
Ian Marte <ianmarte1306@gmail.com>
2026-04-25 17:31:09 +0100

    clari.py

--- clari.py
+++ clari.py
@@ -5,13 +5,6 @@ st.set_page_config(page_title="Mi registro de productos", page_icon="🛒")
 
 st.title("🛒 Registro de productos")
 
-import streamlit as st
-import pandas as pd
-
-st.set_page_config(page_title="Mi registro de productos", page_icon="🛒")
-
-st.title("🛒 Registro de productos")
-
 st.markdown("""
 <style>
 
@@ -43,45 +36,6 @@ button[kind="header"] {
 
 </style>
 """, unsafe_allow_html=True)
-
-# Meoria temporal por el momento--------------------------------------------
-if "productos" not in st.session_state:
-    st.session_state.productos = []
-
-# Formulario
-with st.form("formulario"):
-    nombre = st.text_input("Nombre del producto")
-    precio = st.number_input("precio unitario", min_value=0.0, format="%.2f")
-    cantidad = st.number_input("cantidad", min_value=1, step=1)
-
-    guardar = st.form_submit_button("Registrar producto")
-
-    if guardar:
-        total = precio * cantidad
-
-        st.session_state.productos.append({
-            "Producto": nombre,
-            "Precio": precio,
-            "Cantidad": cantidad,
-            "Total": total
-        })  
-
-        st.success("producto agregado")
-
-#Mostrar la tabla--------
-if st.session_state.productos:
-    st.subheader("📦Productos registrados")
-
-    df = pd.DataFrame(st.session_state.productos)
-    st.dataframe(df, use_container_width=True)
-
-    suma_total = df["Total"].sum()
-
-    st.metric("💰 Total General", f"${suma_total:.2f}")
-
-if st.button("🗑️ Limpiar todo"):
-   st.session_state.productos = []
-   st.rerun()            
 
 # Meoria temporal por el momento--------------------------------------------
 if "productos" not in st.session_state:
